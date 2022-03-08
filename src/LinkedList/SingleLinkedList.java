b3cd9d65b76266a9d7f76a6d76a69498ce19d4a5
nect2r <nect2r@kakao.com>
2022-03-08 23:45:25 +0900

    링크드리스트 삭제,중간삽입 심화 예제

--- src/LinkedList/SingleLinkedList.java
+++ src/LinkedList/SingleLinkedList.java
@@ -38,4 +38,52 @@ public class SingleLinkedList<T> {
         }
     }
 
+    public Node<T> search(T data) {
+        if(this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while(node != null) {
+                if(node.data == data) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public void addNodeInside(T data, T isData) {
+        Node<T> searchedNode = this.search(isData);
+
+        if(searchedNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data);
+            searchedNode.next.next = nextNode;
+        }
+    }
+
+    public boolean delNode(T isData) {
+        if(this.head == null) {
+            return false;
+        } else {
+            Node<T> node = this.head;
+            if(node.data == isData) {
+                this.head = this.head.next;
+                return true;
+            } else {
+                while(node.next != null) {
+                    if(node.next.data == isData) {
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+    }
 }
