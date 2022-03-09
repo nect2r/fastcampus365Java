dddad7d5d3590767f7aa69e563cf5442d8ff53b5
nect2r <nect2r@kakao.com>
2022-03-09 23:57:47 +0900

    이중링크드 리스트 기본

---
+++ src/LinkedList/DoubleLinkedList.java
@@ -0,0 +1,43 @@
+package LinkedList;
+
+public class DoubleLinkedList<T> {
+    public Node<T> head = null;
+    public Node<T> tail = null;
+
+    public class Node<T> {
+        T data;
+        Node<T> prev = null;
+        Node<T> next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public void addNode(T data) {
+        if(this.head == null) {
+            this.head = new Node<T>(data);
+            this.tail = this.head;
+        } else {
+            Node<T> node = this.head;
+            while(node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+            node.next.prev = node;
+            this.tail = node.next;
+        }
+    }
+
+    public void printAll() {
+        if(this.head != null) {
+            Node<T> node = this.head;
+            System.out.println(node.data);
+            while(node.next != null) {
+                node = node.next;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+}
